2aca06ca649526b918645092b749df1c398ecf00
Lucas Bäckman <lucas.backman@gmail.com>
2022-05-02 11:28:41 +0300

    Add interactivity

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,11 +1,4 @@
-# Test arrays
-a1 = [17, 3, 6, 9, 15, 8, 6, 1, 10]
-a2 = [3, 2, 2, 1, 2, 5, 3, 4, 2]
-a3 = [6, 2, 4, 1]
-a4 = [4, 2, 1]
-a5 = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
-
-# Principle converts array with numbers to an array of differences
+# Principle: converts array with numbers to an array of differences
 # Ex. [17, 3, 6, 9, 15, 8, 6, 1, 10] => [-14, 3, 3, 6, -7, -2, -5, 9]
 # Then find the run of digits with the highest sum
 # In the above example: 3 + 3 + 6 = 12
@@ -15,14 +8,14 @@ a5 = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
 def stock_picker(array)
   arr = []
   if strict_decrease?(array)
-    p "#{arr}, do not buy"
+    "#{arr}, do not buy"
   else
     differences = array_of_diffs(array)
     candidates = get_iterations_array(differences)
     ideal = find_max_sum(candidates)
     arr[0] = ideal[:start_index]
     arr[1] = ideal[:end_index]
-    p "#{arr}, for a profit of $#{array[arr[1]]} - $#{array[arr[0]]} == $#{ideal[:sum]}"
+    "#{arr}, for a profit of $#{array[arr[1]]} - $#{array[arr[0]]} == $#{ideal[:sum]}"
   end
 end
 
@@ -66,13 +59,7 @@ def find_min_length(arr)
   min_length = arr.min_by { |hash| hash[:length] }
 end
 
-p a1
-stock_picker(a1)
-p a2
-stock_picker(a2)
-p a3
-stock_picker(a3)
-p a4
-stock_picker(a4)
-p a5
-stock_picker(a5)
+p "Enter an array of integers representing stock values for different days"
+arr = gets.chomp.split(" ").compact.map { |n| n = n.to_i}
+p "Input array: #{arr}"
+p "Best strategy: #{stock_picker(arr)}"
